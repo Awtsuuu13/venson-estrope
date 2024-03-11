3f14b99770f3f14f1686178b90ab5e3baa1c17e1
Awtsuuu13 <vensonestrope13@gmail.com>
2024-03-11 16:54:22 +0800

    Adding About

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import About from './components/about/About'
 import Header from './components/header/Header'
 import Home from './components/home/Home'
 
@@ -10,6 +11,7 @@ function App() {
 
       <main className="main">
         <Home />
+        <About />
       </main>
     </>
   )
