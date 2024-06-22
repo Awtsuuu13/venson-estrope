edda21d73ff45c8747354a53856d0ddf6b4ddd3a
Venson Estrope <vensonestrope13@gmail.com>
2024-06-23 00:03:40 +0800

    Test Img Url

--- src/components/work/WorkItems.jsx
+++ src/components/work/WorkItems.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 function WorkItems({item}) {
   return (
     <div className='work__card' key={item.id}>
-        <img src={item.image} alt="" className='work__img'/>
-        <h3 className="work__title">{item.title}</h3>
-        <a href="" className="work__button">
+        <a href={item.link} target="_blank" rel="noopener noreferrer">
+          <img src={item.image} alt="" className='work__img'/>
+          <h3 className="work__title">{item.title}</h3>
+        </a>
+        <a href="#" className="work__button">
             {/* Demo <i className="bx bx-right-arrow-alt work__button-icon"></i> */}
         </a>
 
